58614717f90cdd9b186ccb892c1bc4c4b9b3b864
yanzhao <yanzhaoyang47@gmail.com>
2022-12-23 21:39:34 -0800

    added sortMerge.c and textfileBulkCreator.py

---
+++ algorithms/sortMerge.c
@@ -0,0 +1,98 @@
+/*
+Gets ints from user and returns merge sorted array.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// prototype of function to implement merge sort
+void merge_sort(int array[], int start, int end, int auxArray[]);
+
+int main(void)
+{
+    // declare array[] to be sorted and auxArray[]
+    int array[100], auxArray[100];
+
+    // get size of array from user
+    int n;
+    printf("Enter size of array: ");
+    scanf("%i", &n);
+
+    // get integers in array from user
+    printf("Enter %i integers: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%i", &array[i]);
+
+    // merge sort array
+    merge_sort(array, 0, n - 1, auxArray);
+
+    // print sorted array
+    printf("[");
+    for (int i = 0; i < n; i++)
+        printf(" %i ", array[i]);
+    printf("]\n");
+
+    return 0;
+}
+
+
+void merge_sort(int array[], int start, int end, int auxArray[])
+{
+    // base case: if (sub)array is of size 1 or is empty, consider the array sorted and return
+    if (end <= start)
+        return;
+
+    // find the middle of the array
+    int mid = (start + end) / 2; // note only integer is returned, not decimals
+
+    // recursively sort...
+    // ... left sub-array, which is from array[start ... mid]
+    merge_sort(array, start, mid, auxArray);
+    // ... right sub-array, which is from array[mid + 1, ... end]
+    merge_sort(array, mid + 1, end, auxArray);
+
+    // declare pointers for the beginnings of the left and right sub-arrays
+    int pointerLeft = start;
+    int pointerRight = mid + 1;
+
+    // loop from start to end to fill each element of auxArray[]
+    for (int i = start; i < end + 1; i++)
+    {
+        // if pointerLeft points to the beginning of the right sub-array
+        if (pointerLeft == mid + 1)
+        {
+            auxArray[i] = array[pointerRight];
+            pointerRight++;
+        }
+        // if pointerRight points one past the end of the right sub-array
+        else if (pointerRight == end + 1)
+        {
+            auxArray[i] = array[pointerLeft];
+            pointerLeft++;
+        }
+        // if pointerLeft points to the smaller element
+        else if (array[pointerLeft] < array[pointerRight])
+        {
+            auxArray[i] = array[pointerLeft];
+            pointerLeft++;
+        }
+        // if pointerRight points to the smaller element
+        else if (array[pointerLeft] > array[pointerRight])
+        {
+            auxArray[i] = array[pointerRight];
+            pointerRight++;
+        }
+        // if pointerRight and pointerLeft point to values of the same value
+        else
+        {
+            auxArray[i] = array[pointerLeft];
+            pointerLeft++;
+        }
+    }
+
+    // copy elements from auxArray[] to array[]
+    for (int i = 0; i < end + 1; i++)
+    {
+        array[i] = auxArray[i];
+    }
+}
---
+++ mini_programs/textfileBulkCreator.py
@@ -0,0 +1,68 @@
+"""
+Textfile Bulk Creator
+Yanzhao Yang
+
+Loops through a CSV file, reads specific values, then finds and edits corresponding strings in a text file. Useful for the bulk creation of unit-specific router config files, etc.
+Adapted from code written for a job, with the variables and fields changed (but is functionally identical).
+"""
+
+import sys
+import csv
+import io
+
+# Ensure correct usage
+if len(sys.argv) != 4:
+    sys.exit("Usage: py textfileBulkCreator.py fileTemplate1 fileTemplate2 csvFile")
+
+# Read unitData into memory from file
+unitData = []
+with open(sys.argv[1]) as csvFile:
+    # Create csv.Dictreader reader
+    reader = csv.DictReader(ipFile)
+    for unit in reader:
+        unitData.append(unit)
+
+# Loop through all units in unitData
+for i in range(len(unitData)):
+    unit = unitData[i]
+
+    # Open fileTemplate1
+    with open(sys.argv[2], 'r') as fileTemplate1:
+        # Read every line
+        fileTemplate1Temp = fileTemplate1.read()
+
+        # Replace unitID
+        unitID = unit["Unit ID Column"]
+        fileTemplate1Temp = fileTemplate1Temp.replace("xxxx", unitID)
+
+        # Replace IP
+        ip = unit["Unit IP Column"].lstrip("10.128.")
+        fileTemplate1Temp = fileTemplate1Temp.replace("yy.zz", ip)
+
+        # Replace S/N
+        unitNumber = unit["Unit Number Column"]
+        snL = unitNumber[0:2]
+        snR = unitNumber[2:]
+        sn = f"{snL}.{snR}"
+        fileTemplate1Temp = fileTemplate1Temp.replace("vv.vv", sn)
+
+    # Write fileTemplate1Temp to fileTemplate1Out
+    with open(f"{unitNumber}_router.txt", "w") as fileTemplate1Out:
+        fileTemplate1Out.write(fileTemplate1Temp)
+
+    # Open fileTemplate2
+    with open(sys.argv[3], 'r') as fileTemplate2:
+        # Read every line
+        fileTemplate2Temp = fileTemplate2.read()
+
+        # Replace unitID
+        unitID = unit["Unit ID Column"]
+        fileTemplate2Temp = fileTemplate2Temp.replace("xxxx", unitID)
+
+        # Replace IP
+        ip = unit["Unit IP Column"].lstrip("10.128.")
+        fileTemplate2Temp = fileTemplate2Temp.replace("yy.zz", ip)
+
+    # Write fileTemplate2Temp to fileTemplate2Out
+    with open(f"{unitNumber}_wifi.txt", "w") as fileTemplate2Out:
+        fileTemplate2Out.write(fileTemplate2Temp)
